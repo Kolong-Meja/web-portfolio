956c8141dfbafc93214ab549aa9debd27631243d
Faisal Ramadhan <faisalramadhan1299@gmail.com>
2025-05-18 12:17:01 +0700

    change on skills sections

---
+++ src/lib/components/SkillsBadge.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let value: string;
+</script>
+
+<div class="bg-soft-dark rounded-full px-3 py-1.5 sm:px-4 sm:py-2 dark:bg-zinc-900">
+	<span class="text-base font-medium text-gray-50 sm:text-lg dark:text-white">{value}</span>
+</div>
--- src/lib/components/sections/Skill.svelte
+++ src/lib/components/sections/Skill.svelte
@@ -4,26 +4,27 @@
 	import 'highlight.js/styles/github-dark-dimmed.css';
 	import { onMount } from 'svelte';
 	import TypeIt from 'typeit';
+	import SkillsBadge from '../SkillsBadge.svelte';
+	import { t } from '$lib/translations';
 
 	const value = `const skills = [
-  "Time Management",
-  "Attention to Detail",
-  "Adaptability",
-  "Problem Solving",
-  "Restful API Development",
-  "Database Management Software",
-  "Backend Development",
-  "Frontend Development",
-  "ERP Software Development",
-  "Version Control Systems",
-  "Linux Environments",
+  "Laravel",
+  "Spring Boot",
+  "NestJS",
+  "Next.js",
+  "Vue.js",
+  "Sveltekit",
+  "Git",
   "Docker",
-  "Message Broker",
-  "Microservices",
-  "NoSQL Database",
+  "PostgreSQL",
+  "Typescript",
   "Java",
   "PHP",
-  "Javascript",
+  "Go",
+  "Node.js",
+  "Restful API",
+  "Microservices",
+  "Linux",
 ];
 
 function expertise(skills: Array<string>): Array<string> {
@@ -40,142 +41,98 @@ function main() {
 main();
 `;
 
-	const otherValue = `import (
-	"cmp"
-	"fmt"
-	"slices"
-)
-
-func main() {
-	skills := []string{
-		"Time Management",
-		"Attention to Detail",
-		"Adaptability",
-		"Problem Solving",
-		"Restful API Development",
-		"Database Management Software",
-		"Backend Development",
-		"Frontend Development",
-		"ERP Software Development",
-		"Version Control Systems",
-		"Linux Environments",
-		"Docker",
-		"Message Broker",
-		"Microservices",
-		"NoSQL Database",
-		"Java",
-		"PHP",
-		"Javascript",
-	}
-
-	e := expertise(skills)
-
-	for i := range e {
-		fmt.Println(e[i])
-	}
-}
-
-func expertise(skills []string) []string {
-	customSort := func(a, b string) int {
-		return cmp.Compare(a, b)
-	}
-
-	slices.SortFunc(skills, customSort)
-
-	return skills
-}
-`;
+	const skills = [
+		'Laravel',
+		'Spring Boot',
+		'NestJS',
+		'Next.js',
+		'Vue.js',
+		'Sveltekit',
+		'Git',
+		'Docker',
+		'PostgreSQL',
+		'Typescript',
+		'Java',
+		'PHP',
+		'Go',
+		'Node.js',
+		'Restful API',
+		'Microservices',
+		'Linux'
+	];
 
 	onMount(() => {
 		const panel = document.querySelector('pre #code') as HTMLElement;
-		const otherPanel = document.querySelector('pre #other-code') as HTMLElement;
 
 		hljs.highlightElement(panel);
-		hljs.highlightElement(otherPanel);
 		hljs.initHighlightingOnLoad();
 
 		const codeTypeIt = new TypeIt('#code', {
-			speed: 30,
+			speed: 40,
 			waitUntilVisible: true,
 			loop: false,
 			lifeLike: false
 		});
 		codeTypeIt.go();
-
-		const otherCodeTypeIt = new TypeIt('#other-code', {
-			speed: 30,
-			waitUntilVisible: true,
-			loop: false,
-			lifeLike: false
-		});
-		otherCodeTypeIt.go();
 	});
 </script>
 
 <section
 	id="skill"
-	class="dark:bg-soft-black max-h-full min-h-screen w-screen max-w-full bg-black transition-colors duration-300 ease-in-out"
+	class="dark:bg-soft-black font-hanken-grotesk max-h-full min-h-screen w-screen max-w-full bg-black transition-colors duration-300 ease-in-out"
 >
 	<div class="container mx-auto">
-		<div
-			class="flex w-full flex-col items-center space-y-4 px-4 py-8 sm:px-5 sm:py-12 md:px-6 md:py-16 lg:flex-row lg:space-y-0 lg:space-x-4 lg:px-7 lg:py-20 xl:px-8 xl:py-24"
-		>
-			<!-- Left Panel Code -->
-			<pre
-				class="bg-soft-black dark:bg-soft-dark relative flex w-full flex-col rounded-xl transition-colors duration-300 ease-in-out lg:w-[35rem] xl:w-[40rem]">
-      <img
-					id="logo"
-					src={AstronautFigure}
-					alt="Astronaut Pixel Icon"
-					class="show-on-scroll absolute top-[-3rem] left-3 h-[3rem] w-[3rem] md:top-[-4rem] md:left-4 md:h-[4rem] md:w-[4rem]"
-					loading="lazy"
-				/>
-      <img
-					id="logo"
-					src={AlienFigure}
-					alt="Astronaut Pixel Icon"
-					class="show-on-scroll absolute top-[-3rem] right-20 h-[3rem] w-[3.5rem] md:top-[-4rem] md:right-32 md:h-[4rem] md:w-[4.5rem]"
-					loading="lazy"
-				/>
-      <img
-					id="logo"
-					src={UFOFigure}
-					alt="Astronaut Pixel Icon"
-					class="show-on-scroll absolute top-[-2rem] right-10 h-[2rem] w-[2.5rem] md:top-[-3rem] md:right-16 md:h-[3rem] md:w-[3.5rem]"
-					loading="lazy"
-				/>
-        <div class="flex flex-row items-center justify-between p-2">
-          <p class="text-base text-white">skills.ts</p>
-          <div class="flex flex-row items-center space-x-2">
-            <div class="h-3 w-3 rounded-full bg-red-400 dark:bg-rose-400"></div>
-            <div class="h-3 w-3 rounded-full bg-yellow-400 dark:bg-orange-300"></div>
-            <div class="h-3 w-3 rounded-full bg-green-400 dark:bg-emerald-400"></div>
+		<div class="px-4 py-8 sm:px-5 sm:py-12 md:px-6 md:py-16 lg:px-7 lg:py-20 xl:px-8 xl:py-24">
+			<p
+				class="group font-space-grotesk relative mb-6 w-full text-center text-xl font-bold text-emerald-300 md:text-2xl lg:text-4xl xl:mb-12 dark:text-emerald-400"
+			>
+				{$t('content.skills.header')}
+			</p>
+			<div class="flex w-full flex-col items-start space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
+				<!-- Left Panel Code -->
+				<pre
+					class="bg-soft-black dark:bg-soft-dark relative flex w-full flex-col rounded-xl transition-colors duration-300 ease-in-out lg:w-[35rem] xl:w-[40rem]">
+        <img
+						id="logo"
+						src={AstronautFigure}
+						alt="Astronaut Pixel Icon"
+						class="show-on-scroll absolute top-[-3rem] left-3 h-[3rem] w-[3rem] md:top-[-4rem] md:left-4 md:h-[4rem] md:w-[4rem]"
+						loading="lazy"
+					/>
+        <img
+						id="logo"
+						src={AlienFigure}
+						alt="Astronaut Pixel Icon"
+						class="show-on-scroll absolute top-[-3rem] right-20 h-[3rem] w-[3.5rem] md:top-[-4rem] md:right-32 md:h-[4rem] md:w-[4.5rem]"
+						loading="lazy"
+					/>
+        <img
+						id="logo"
+						src={UFOFigure}
+						alt="Astronaut Pixel Icon"
+						class="show-on-scroll absolute top-[-2rem] right-10 h-[2rem] w-[2.5rem] md:top-[-3rem] md:right-16 md:h-[3rem] md:w-[3.5rem]"
+						loading="lazy"
+					/>
+          <div class="flex flex-row items-center justify-between p-2">
+            <p class="text-base text-white">skills.ts</p>
+            <div class="flex flex-row items-center space-x-2">
+              <div class="h-3 w-3 rounded-full bg-red-400 dark:bg-rose-400"></div>
+              <div class="h-3 w-3 rounded-full bg-yellow-400 dark:bg-orange-300"></div>
+              <div class="h-3 w-3 rounded-full bg-green-400 dark:bg-emerald-400"></div>
+            </div>
           </div>
-        </div>
-        <code
-					id="code"
-					class="language-typescript h-[25rem] overflow-x-auto rounded-b-xl text-sm text-wrap md:text-base"
-					>{value}</code
-				>
-    </pre>
-
-			<!-- Right Panel Code -->
-			<pre
-				class="bg-soft-black dark:bg-soft-dark relative flex w-full flex-col rounded-xl transition-colors duration-300 ease-in-out lg:w-[35rem] xl:w-[40rem]">
-      <div class="flex flex-row items-center justify-between p-2">
-        <p class="text-base text-white">skills.go</p>
-        <div class="flex flex-row items-center space-x-2">
-          <div class="h-3 w-3 rounded-full bg-red-400 dark:bg-rose-400"></div>
-          <div class="h-3 w-3 rounded-full bg-yellow-400 dark:bg-orange-300"></div>
-          <div class="h-3 w-3 rounded-full bg-green-400 dark:bg-emerald-400"></div>
-        </div>
-      </div>
-      <code
-					id="other-code"
-					class="language-go h-[25rem] overflow-x-auto rounded-b-xl text-sm text-wrap md:text-base"
-					>{otherValue}</code
-				>
-  </pre>
+          <code
+						id="code"
+						class="language-typescript h-[25rem] overflow-x-auto rounded-b-xl text-sm text-wrap md:text-base"
+						>{value}</code
+					>
+      </pre>
+				<div class="flex flex-wrap items-center gap-3 sm:gap-4 p-2 lg:w-[35rem] xl:w-[40rem]">
+					{#each skills as skill}
+						<SkillsBadge value={skill} />
+					{/each}
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
